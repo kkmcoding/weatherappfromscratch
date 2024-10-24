c7191070cbcd6c33c5887ea8232b6331d10d766a
kkmcoding <kutlwanokatlegomoshugi@gmail.com>
2024-10-24 11:22:19 +0200

    cleaning up search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature-now");
   temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
-
 function searchCity(city) {
   let apiKey = "6td3328233211ee5oa4079e6eef2f07b";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -13,13 +14,10 @@ function searchCity(city) {
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-field");
-  let city = document.querySelector("#city");
-  if (searchInput.value) {
-    city.innerHTML = `${searchInput.value}`;
-  } else {
-    alert("Please type a city");
-  }
   searchCity(searchInput.value);
 }
+
 let searchFormInput = document.querySelector("#search-form");
 searchFormInput.addEventListener("submit", search);
+
+searchCity("London");
