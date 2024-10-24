dd265ff06f39f9d4137d293bc900ae7a54cf8f6a
kkmcoding <kutlwanokatlegomoshugi@gmail.com>
2024-10-24 11:02:24 +0200

    adding axios and apiURL  & changing innerHTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-now");
+  temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "6td3328233211ee5oa4079e6eef2f07b";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-field");
@@ -5,9 +17,9 @@ function search(event) {
   if (searchInput.value) {
     city.innerHTML = `${searchInput.value}`;
   } else {
-    city.innerHTML = null;
     alert("Please type a city");
   }
+  searchCity(searchInput.value);
 }
 let searchFormInput = document.querySelector("#search-form");
 searchFormInput.addEventListener("submit", search);
